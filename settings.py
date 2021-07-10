496bbf889179458607b1cc3e2b1ac9ebc263d226
Bohan Zhang <bohanzhang@192.168.2.17>
2021-07-10 20:59:52 +0800

    added aliens and play button

--- settings.py
+++ settings.py
@@ -2,18 +2,40 @@ class Settings:
     """存储游戏《外星人入侵》中所有设置的类。"""
 
     def __init__(self):
-        """初始化游戏的设置。"""
+        """初始化游戏的静态设置。"""
         # 屏幕设置。
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
         # 飞船设置。
-        self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # 子弹设置。
-        self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullet_allowed = 3
+        self.bullet_allowed = 100
+
+        # 外星人设置。
+        self.fleet_drop_speed = 10
+
+        # 加快游戏节奏的速度。
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行而变化的设置。"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet_direction为1表示向右移，为-1表示向左移。
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """提高速度设置。"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
